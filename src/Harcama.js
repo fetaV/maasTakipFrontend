70fc73615f762309c76583b847c637230186d29d
fetaV <vatef06@gmail.com>
2024-06-07 21:58:34 +0300

    tarih dropdown ekleniyor

--- src/Harcama.js
+++ src/Harcama.js
@@ -469,16 +469,51 @@ function Harcama() {
             <li className="list-group-item d-flex justify-content-between align-items-center">
               <table className="table">
                 <thead>
-                  <div className="d-flex">
-                    <input
-                      className="form-control mt-2"
-                      type="search"
-                      placeholder="Ara"
-                      aria-label="Search"
-                      value={searchTerm}
-                      onChange={e => setSearchTerm(e.target.value)}
-                    />
-                  </div>
+                  <tr>
+                    <th>
+                      <div className="dropdown">
+                        <button
+                          className="btn btn-secondary dropdown-toggle"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          Dropdown button
+                        </button>
+                        <ul className="dropdown-menu">
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              Action
+                            </a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              Another action
+                            </a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              Something else here
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </th>
+                    <th>
+                      <input
+                        className="form-control mt-2"
+                        type="search"
+                        placeholder="Ara"
+                        aria-label="Search"
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                      />
+                    </th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                  </tr>
                 </thead>
                 <thead>
                   <tr>
